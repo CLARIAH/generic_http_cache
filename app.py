12910efcb014d40a40b3a511b086f2ba640f2841
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2025-03-10 17:50:10 +0100

    add log info; return according to accept passed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, HTTPException, Query, Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, HTTPException, Query, Request, Response
+from fastapi.responses import JSONResponse, PlainTextResponse
 from urllib.parse import urlparse, unquote, parse_qs
 import requests
 import cache_settings as settings
@@ -14,37 +14,43 @@ app = FastAPI()
 cache = settings.cache_type()
 cache.init(**settings.kwargs)
 
+
 @app.get("/")
 def index():
     return {"message": "Hello, World!"}
+
 
 @app.get("/proxy")
 def proxy(request: Request, url: str = Query(...)):
     full_requested_url = str(request.url)
     full_requested_url_parsed = urlparse(full_requested_url)
     query_params = parse_qs(full_requested_url_parsed.query)
-    logger.warning(f"query_params: {query_params}")
+    logger.info(f"Query params: {query_params.get('accept', None)[0]}")
 
     decoded_url = unquote(url)
     parsed_url = urlparse(decoded_url)
-    accept_header = query_params.get(settings.accept_header_key, [None])[0]
+    accept_header = query_params.get('accept', None)[0]
 
     if not parsed_url.scheme or not parsed_url.netloc:
         raise HTTPException(status_code=400, detail="Invalid URL")
 
     cached_content = cache.get(key=decoded_url)
     if cached_content:
-        return {"source": "cache", "content": cached_content}
+        logger.info(f"Cache hit for {decoded_url}")
+        logger.info(f"Accept header: {accept_header}")
+        logger.debug(f"Cache Content: {cached_content}")
+        return Response(content=cached_content, media_type=accept_header)
 
     try:
-        headers = {"Accept": accept_header} if accept_header else {}
-        logger.warning(f"Requesting {decoded_url}")
-        logger.warning(f"headers: {headers}")
+        logger.info(f"Cache miss for {decoded_url}")
+        logger.info(f"Accept header: {accept_header}")
+        headers = {"Accept": accept_header} if accept_header else request.headers.get("accept")
         response = requests.get(decoded_url, headers=headers)
         response.raise_for_status()
-        content = response.text
-        cache.set(key=decoded_url, value=content, ttl=settings.ttl)
-        return {"source": "origin", "content": content}
+        content = response.content
+        cache.set(key=decoded_url, value=content.decode('utf-8'), ttl=settings.ttl)
+        logger.debug(f"Origin Content: {content}")
+        return Response(content=content, media_type=accept_header)
     except requests.RequestException as e:
         raise HTTPException(status_code=502, detail=str(e))
 
